96e6eb421361e4d6b80e09a169bf02e1262dba12
thorc <chelsgieck@gmail.com>
2018-06-16 01:24:20 -0400

    remove justin password and update package.json

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'Ezra0827'
+        'password': ''
     },
 	'database': 'cheftest1',
     'users_table': 'users'
